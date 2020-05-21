f8b1d26dd04713fda2faa51c81e5945f9891c289
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-21 23:01:57 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,32 +1,63 @@
+# class School
+#   attr_accessor :name, :roster
+  
+#   def initialize(name)
+#     @name = name
+#     @roster = {}
+#   end 
+  
+#   def add_student(student, level)
+#     # this sort of means #=> a || a = b ... or #=> x || x = y
+#     # it's freak'n weird
+#     roster[level] ||= []
+#     roster[level] << student
+#   end 
+  
+#   def grade(level)
+#     roster.detect do |x, y| 
+#       if x == level
+#         return y 
+#       end 
+#     end 
+#   end 
+  
+#   def sort 
+#   nu_hash = {}
+#   roster.each do |x, y| 
+#     nu_hash[x] = y.sort 
+#   end 
+#   nu_hash
+# end 
+  
+# end 
+
+
 class School
-  attr_accessor :name, :roster
   
-  def initialize(name)
-    @name = name
+  attr_accessor :roster
+  
+ def initialize(school_name)
+    @name = school_name
     @roster = {}
-  end 
+  end
   
-  def add_student(student, level)
-    # this sort of means #=> a || a = b ... or #=> x || x = y
-    # it's freak'n weird
-    roster[level] ||= []
-    roster[level] << student
-  end 
-  
-  def grade(level)
-    roster.detect do |x, y| 
-      if x == level
-        return y 
-      end 
-    end 
-  end 
-  
-  def sort 
-  nu_hash = {}
-  roster.each do |x, y| 
-    nu_hash[x] = y.sort 
-  end 
-  nu_hash
-end 
-  
-end 
+  def add_student(name, grade)
+    
+  @roster[grade] = [] if  @roster[grade].nil?
+  @roster[grade].push(name)
+end
+
+def grade(grade)
+  @roster[grade]
+end
+
+def sort
+  @roster.each do |grade, students|
+    students.sort!
+  end
+  @roster
+
+end
+
+end
+    
