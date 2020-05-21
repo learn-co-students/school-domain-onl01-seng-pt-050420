9cdb41c109dcfff05d1af3a262951f50a5297a20
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-05-21 13:55:24 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+    attr_accessor
+    attr_reader :roster
+
+    # initialized with a name.
+    def initialize(roster)
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        roster[grade] ||= [] # does this exist?
+        roster[grade] << student
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        roster_hash = {}
+        roster.each do |grade, student|
+            roster_hash[grade] = student.sort
+        end
+        roster_hash
+    end
+end
