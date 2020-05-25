415dd5f41e746c318930d48d28ceed23b3141b08
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-25 22:56:01 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+
+  attr_accessor :roster, :student
+
+  def initialize(school_name)
+    @name = school_name
+    @roster = { }             # initializing an empty hash
+  end
+
+  def add_student(name, grade)
+    if @roster[grade].nil? #if roster gets nil
+      @roster[grade] = [ ]
+    end
+    @roster[grade].push(name)
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+      students.sort! #the bang sorts in place
+    end
+    @roster
+  end
+
+end
