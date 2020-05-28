5f2a6228af88d658e6e8ab743d58ab09591a8776
Jeff Bai <j.bai@wustl.edu>
2020-05-28 05:56:34 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  #returns whole roster
+  def roster
+    @roster
+  end
+
+  #Returns roster of a grade
+  def grade(grade)
+    @roster[grade]
+  end
+
+  #Add student when called
+  def add_student(name, grade)
+    #check if empty
+    if roster[grade] != nil
+      roster[grade] << name
+    else
+      roster[grade] = [name]
+    end
+  end
+
+  def sort
+    @roster.each do |hash_key, hash_value|
+      hash_value.sort!
+    end
+  end
+end
