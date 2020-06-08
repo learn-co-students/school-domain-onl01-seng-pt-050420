a52774654730d162b9fa5627b067493cc9d905c1
travis Lawson <travislawclimb@gmail.com>
2020-06-08 04:02:14 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,27 +1,35 @@
 class School
-  
-  attr_accessor :roster
-    
-  def initialize(school_name)
-    @name = school_name
-    @roster = {}
-  end 
-  
-  def add_student(name, grade)
-    
-    @roster[grade] =[] if @roster[grade].nil? 
-    @roster[grade].push(name)
-  end  
-  
-  def grade(grade)
-    @roster[grade]
-  end   
-  
-  def sort 
-  @roster.each do |grade, students|
-    students.sort! 
-  end
-  @roster
-end 
 
-end  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
+end
+  
+
+
+school = School.new("Bayside High School")
+
